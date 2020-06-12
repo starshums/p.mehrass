a1778d20d9b1ed5ec36e21539b9081e86724f2db
unknown <chems00rock@gmail.com>
2020-06-12 13:21:33 +0100

    Removed redux dev tools

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -48,18 +48,12 @@ const loadState = () => {
 
 const PERSISTED_STATE = loadState();
 
-const devTools = () => {
-    if( process.env.REACT_APP_ENV !== 'production' )
-        return window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
-    return null;
-}
-
 const store = createStore(
     rootReducer,
     PERSISTED_STATE,
     compose(
         applyMiddleware(thunk),
-        devTools()
+        // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
     )
 );
 
